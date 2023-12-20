e79c2a82a33f7d2158d487d10674390499f8f262
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-20 14:57:03 +0400

    Update Mathsimvol.java

--- Mathsimvol.java
+++ Mathsimvol.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Mathsimvol {
   public static void main(String[] arg) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Vvedite chislo:");
+    System.out.println("Enter the number:");
     int a = sc.nextInt();
-    System.out.println("Vvedite chislo:");
+    System.out.println("Enter the number:");
     int b = sc.nextInt();
     System.out.println(a + "+" + b + "=" + (a + b));
     System.out.println(a + "-" + b + "=" + (a - b));
