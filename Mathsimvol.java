b9ba14302910a81f6e895904012bf52fd8785414
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-20 14:55:59 +0400

    Update Mathsimvol.java

--- Mathsimvol.java
+++ Mathsimvol.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-public class Mathsimvol{
-public static void main(String[] arg){
-Scanner sc=new Scanner(System.in);
-System.out.println("Vvedite chislo:");
-int a=sc.nextInt();
-System.out.println("Vvedite chislo:");
-int b=sc.nextInt();
-System.out.println(a+ "+" +b+ "=" + (a+b));
-System.out.println(a+ "-" +b+ "=" + (a-b));
-System.out.println(a+ "*" +b+ "=" + (a*b));
-System.out.println(a+ "/" +b+ "=" + (a/b));
-System.out.println(a+ "mod" +b+ "=" + (a%b));
+public class Mathsimvol {
+  public static void main(String[] arg) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Vvedite chislo:");
+    int a = sc.nextInt();
+    System.out.println("Vvedite chislo:");
+    int b = sc.nextInt();
+    System.out.println(a + "+" + b + "=" + (a + b));
+    System.out.println(a + "-" + b + "=" + (a - b));
+    System.out.println(a + "*" + b + "=" + (a * b));
+    System.out.println(a + "/" + b + "=" + (a / b));
+    System.out.println(a + "mod" + b + "=" + (a % b));
 
+  }
 }
-}
-
